f497432ba37aca891866cb5366c08e182a55d2a0
Joydeep Nath <joydeepnath279@gmail.com>
2025-10-09 20:11:53 +0530

    add notes

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 
 const app = express();
-app.use("/", (req, res) => {
-  res.send("Server responding root");
-});
 
 app.use("/hello", (req, res) => {
-  res.send("Server responding hello");
+  res.send("Routed /hello");
+});
+
+app.use("/test/get", (req, res) => {
+  res.send("Routed to /test/get");
 });
 
 app.use("/test", (req, res) => {
-  res.send("Server responding test");
+  res.send("Routed to /test");
+});
+
+app.use("/", (req, res) => {
+  res.send("Server responding root");
 });
 
 app.listen(3000, () => {
   console.log("Server Started SuccessFully!");
 });
-
+console.log(app.listen);
