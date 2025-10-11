0ca8ba4f9ce7a48d3ae2ed85429d79c5ff64283e
Joydeep Nath <joydeepnath279@gmail.com>
2025-10-11 11:29:32 +0530

    add notes: http-server,listen and middlewares

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 const express = require("express");
-
 const app = express();
 
-app.use("/hello", (req, res) => {
-  res.send("Routed /hello");
+app.get("/user", (req, res) => {
+  if (req.headers["user-agent"].includes("Postman")) {
+    //request from postman
+    res.send({ Fname: "Joydeep", Lname: "Nath" });
+  } else {
+    //request from other Apps
+    res.send("<h1>Welcome Joydeep Nath</h1>");
+  }
 });
 
-app.use("/test/get", (req, res) => {
-  res.send("Routed to /test/get");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Routed to /test");
+app.get("/test", (req, res) => {
+  res.send("Welcome to test Page");
 });
 
 app.use("/", (req, res) => {
-  res.send("Server responding root");
+  if (req.headers["user-agent"].includes("Postman")) {
+    //request from postman
+    res.send({ status: "Success" });
+  } else {
+    //request from other Apps
+    res.send("<h1>Welcome Joydeep Nath</h1>");
+  }
 });
-
 app.listen(3000, () => {
   console.log("Server Started SuccessFully!");
 });
-console.log(app.listen);
