36e4eb0a8930778eb2f83320624e184a1bc1dfac
Joydeep Nath <joydeepnath279@gmail.com>
2025-11-21 11:24:00 +0530

    fix edit profile

--- src/router/profile.js
+++ src/router/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const auth = require("../middlewares/auth");
+const User = require("../configs/databaseSchema");
 
 route.get("/profile/view", auth, (req, res) => {
   try {
@@ -12,15 +13,20 @@ route.get("/profile/view", auth, (req, res) => {
 
 route.patch("/profile/edit", auth, async (req, res) => {
   try {
-    const allowedUpdate = ["firstName", "lastName", "age", "gender"];
+    const allowedUpdate = ["firstName", "lastName", "profileURL", "gender"];
     const isUpdateAllowed = Object.keys(req.body).every((keys) =>
       allowedUpdate.includes(keys)
     );
     if (!isUpdateAllowed) {
       res.status(403).send("Update not Allowed");
     } else {
-      await User.findByIdAndUpdate(req.params.userID, req.body);
-      res.status(201).send("Success!");
+      console.log(req.userData._id);
+
+      const userData = await User.findByIdAndUpdate(
+        req.userData._id,
+        req.body
+      );
+      res.status(201).send({ userData });
     }
   } catch (error) {
     res.status(400).send(error.message);
